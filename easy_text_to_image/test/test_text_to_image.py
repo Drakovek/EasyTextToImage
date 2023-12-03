407627baf77e86ec88db7c081bc438428019118f
Drakovek <DrakovekMail@gmail.com>
2023-12-03 14:54:42 -0800

    Added functions to create a text image that fits within given dimensions.

--- easy_text_to_image/test/test_text_to_image.py
+++ easy_text_to_image/test/test_text_to_image.py
@@ -132,29 +132,32 @@ def test_get_text_line_image():
     font = etti.get_basic_font("serif", [])
     image = etti.get_text_line_image("Text thing,", font, font_size=20, image_width=300,
             foreground="#0000ffff", background="#ff0000ff", justified="c")
-    assert image.size == (300, 22)
+    assert image.size[0] == 300
+    assert image.size[1] < 28 and image.size[1] > 22
     left, top, right, bottom = etti.get_bounds(image, "#ff0000ff")
-    assert top < 4
+    assert top < 5
     assert bottom > 18
-    assert left < 120
-    assert right > 180
+    assert left < 110
+    assert right > 190
     # Test getting a left justified image
     image = etti.get_text_line_image("123jkl", font, font_size=70, image_width=600, justified="l")
-    assert image.size == (600, 77)
+    assert image.size[0] == 600
+    assert image.size[1] < 85 and image.size[1] > 78
     left, top, right, bottom = etti.get_bounds(image, "#000000ff", foreground=True)
-    assert top < 4
-    assert bottom > 65
-    assert left < 4
-    assert right > 160
+    assert top < 5
+    assert bottom > 60
+    assert left < 5
+    assert right < 200
     #Test getting a right justified image
     image = etti.get_text_line_image("Many Words", font, font_size=32, image_width=400,
                 foreground="#ffff00ff", background="#ff0000ff", justified="r", space=1.5)
-    assert image.size == (400, 48)
+    assert image.size[0] == 400
+    assert image.size[1] < 50 and image.size[1] > 46
     left, top, right, bottom = etti.get_bounds(image, "#ff0000ff")
-    assert top < 4
+    assert top < 5
     assert bottom > 28
-    assert left < 250
-    assert right > 396
+    assert left > 200
+    assert right > 395
     
 def test_get_text_multiline_image():
     """
@@ -165,28 +168,102 @@ def test_get_text_multiline_image():
     image = etti.get_text_multiline_image(["Something", "text and", "such."],
             font, font_size=30, image_width=300, foreground="#0000ffff",
             background="#ff0000ff", justified="c")
-    assert image.size == (300, 99)
+    assert image.size[0] == 300
+    assert image.size[1] < 110 and image.size[1] > 95
     left, top, right, bottom = etti.get_bounds(image, "#ff0000ff")
     assert top < 4
-    assert bottom > 80
-    assert left < 100
-    assert right > 200
+    assert bottom > 86
+    assert left < 85
+    assert right > 215
     # Test getting a left justified image
     image = etti.get_text_multiline_image(["More things", "Stuff to read."],
             font, font_size=20, image_width=200, justified="l")
-    assert image.size == (200, 44)
+    assert image.size[0] == 200
+    assert image.size[1] < 55 and image.size[1] > 45
     left, top, right, bottom = etti.get_bounds(image, "#000000ff", foreground=True)
-    assert top < 4
-    assert bottom > 30
-    assert left < 4
-    assert right > 90
+    assert top < 5
+    assert bottom > 38
+    assert left < 5
+    assert right < 130
     # Test getting a right justified image
     image = etti.get_text_multiline_image(["A", "B", "C", "D", "Yet More Things"],
             font, font_size=72, image_width=700, foreground="#00aa00ff",
             background="#000000ff", justified="r", space=1.5)
-    assert image.size == (700, 540)
+    assert image.size[0] == 700
+    assert image.size[1] < 530 and image.size[1] > 520
     left, top, right, bottom = etti.get_bounds(image, "#000000ff")
+    assert top < 12
+    assert bottom > 485
+    assert left > 150
+    assert right > 695
+
+def test_get_word_wrap():
+    """
+    Tests the get_word_wrap function.
+    """
+    # Test if the minimum character value is less than the largest word.
+    font = etti.get_basic_font("serif", [])
+    lines, size = etti.get_word_wrap("This is an example sentence!!!", font, image_width=300, minimum_characters=4)
+    assert lines == ["This is an", "example", "sentence!!!"]
+    assert size > 58 and size < 64
+    # Test if the minimum character value is greater
+    font = etti.get_basic_font("serif", [])
+    lines, size = etti.get_word_wrap("Just one line.", font, image_width=300, minimum_characters=50)
+    assert lines == ["Just one line."]
+    assert size > 46 and size < 54
+
+def test_text_image_fit_width():
+    """
+    Tests the text_image_fit_width function.
+    """
+    font = etti.get_basic_font("serif", [])
+    text = "Here is a decent number of words."
+    image = etti.text_image_fit_width(text, font, image_width=300,
+            foreground="#101010ff", background="#0000aaff",
+            justified="l")
+    assert image.size[0] == 300
+    assert image.size[1] > 540 and image.size[1] < 565
+    left, top, right, bottom = etti.get_bounds(image, "#0000aaff")
     assert top < 4
-    assert bottom > 480
-    assert left < 500
-    assert right > 696
+    assert bottom > 540
+    assert left < 4
+    assert right > 285
+
+def test_text_image_fit_box():
+    """
+    Tests the text_image_fit_box function.
+    """
+    # Test aligning text to the top of the image
+    font = etti.get_basic_font("serif", [])
+    text = "Here is a fairly long string of text to format into a word-wrapped image."
+    image = etti.text_image_fit_box(text, font, image_width=300, image_height=400,
+            foreground="#ffffffff", background="#300000ff",
+            justified="c", vertical="t")
+    assert image.size == (300, 400)
+    left, top, right, bottom = etti.get_bounds(image, "#300000ff")
+    assert top < 4
+    assert bottom < 385
+    assert left < 6
+    assert right > 294
+    # Test aligning text to the bottom of the image
+    font = etti.get_basic_font("serif", [])
+    image = etti.text_image_fit_box(text, font, image_width=300, image_height=200,
+            foreground="#010101ff", background="#aaaaaaff",
+            justified="l", vertical="b")
+    assert image.size == (300, 200)
+    left, top, right, bottom = etti.get_bounds(image, "#aaaaaaff")
+    assert top > 50
+    assert bottom > 195
+    assert left < 5
+    assert right > 290
+     # Test aligning text vertically to the center of the image
+    font = etti.get_basic_font("serif", [])
+    image = etti.text_image_fit_box(text, font, image_width=300, image_height=150,
+            foreground="#bb0000ff", background="#202020ff",
+            justified="r", vertical="c")
+    assert image.size == (300, 150)
+    left, top, right, bottom = etti.get_bounds(image, "#202020ff")
+    assert top < 15
+    assert bottom > 135
+    assert left < 8
+    assert right > 292
--- easy_text_to_image/text_to_image.py
+++ easy_text_to_image/text_to_image.py
@@ -2,10 +2,13 @@
 
 import re
 import math
+import textwrap
 import metadata_magic.file_tools as mm_file_tools
 from PIL import Image, ImageColor, ImageDraw, ImageFont
 from os.path import abspath, basename, exists, expandvars, join
 from typing import List
+
+TEXT_REF = "ÅBCDÉFGHIJKLMNÖPQRSTÜVWXYZ1234567890abcdefghijklmnopqrstuvwxyz"
 
 def get_font_locations() -> List[str]:
     """
@@ -160,7 +163,7 @@ def get_bounds(image:Image, color:str, foreground:bool=False) -> (int, int, int,
     
 def get_text_line_image(text:str, font:ImageFont, font_size:int,
             image_width:int, foreground:str="#000000ff", background:str="#ffffff00",
-            space:float=1.1, justified:str="c") -> Image:
+            space:float=1.2, justified:str="c") -> Image:
     """
     Creates an image containing text in one continuous line.
     
@@ -170,14 +173,16 @@ def get_text_line_image(text:str, font:ImageFont, font_size:int,
     :type font: PIL.ImageFont, required
     :param font_size: Size of the font height in pixels
     :type font_size: int, required
-    :param foreground: Width of the generated image in pixels
-    :type foreground: int required
+    :param image_width: Width of the generated image in pixels
+    :type image_width: int required
+    :param foreground: Hex color value of the text in the generated image, defaults to "#000000ff"
+    :type foreground: str, optional
     :param background: Hex color value of the generated image backround, defaults to "#ffffff00"
     :type background: str, optional
-    :param justified: Hex color value of the text in the generated image, defaults to "#000000ff"
-    :type justified: str, optional
     :param space: Multiplied by font_size for space under the text, defaults to 1.25
     :type space: float, optional
+    :param justified: How to align the image ("l":left, "c":center, "r":right), defaults to "c"
+    :type justified: str, optional
     :return: Image with the given text on one line
     :rtype: PIL.Image
     """
@@ -187,11 +192,16 @@ def get_text_line_image(text:str, font:ImageFont, font_size:int,
     altered_font = font.font_variant(size=font_size)
     # Print the text on the image
     draw = ImageDraw.Draw(image)
-    draw.text(xy=(font_size,font_size), text=text, fill=foreground, font=altered_font)
+    draw.text(xy=(1, 1), text=text, fill=foreground, font=altered_font)
+    # Get reference text bounding box
+    rl, ref_top, rr, ref_bottom = altered_font.getbbox(TEXT_REF)
+    ref_bottom +=1
     # Crop the image
-    image = image.crop(get_bounds(image, "#00000000"))
+    left, top, right, bottom = get_bounds(image, "#00000000")
+    image = image.crop((left, ref_top, right+1, ref_bottom))
     # Create the backround image with the right size
-    background = Image.new("RGBA", size=(image_width, math.floor(font_size*space)), color=background)
+    image_height = math.floor((ref_bottom - ref_top) * space)
+    background = Image.new("RGBA", size=(image_width, image_height), color=background)
     # Get the points in which to past the text
     text_width, text_height = image.size
     y = 1
@@ -208,7 +218,7 @@ def get_text_line_image(text:str, font:ImageFont, font_size:int,
 
 def get_text_multiline_image(lines:List[str], font:ImageFont, font_size:int,
             image_width:int, foreground:str="#000000ff", background:str="#ffffff00",
-            space:float=1.1, justified:str="c") -> Image:
+            space:float=1.2, justified:str="c") -> Image:
     """
     Creates an image containing text in multiple lines
     
@@ -218,19 +228,24 @@ def get_text_multiline_image(lines:List[str], font:ImageFont, font_size:int,
     :type font: PIL.ImageFont, required
     :param font_size: Size of the font height in pixels
     :type font_size: int, required
-    :param foreground: Width of the generated image in pixels
-    :type foreground: int required
+    :param image_width: Width of the generated image in pixels
+    :type image_width: int required
+    :param foreground: Hex color value of the text in the generated image, defaults to "#000000ff"
+    :type foreground: str, optional
     :param background: Hex color value of the generated image backround, defaults to "#ffffff00"
     :type background: str, optional
-    :param justified: Hex color value of the text in the generated image, defaults to "#000000ff"
-    :type justified: str, optional
-    :param space: Multiplied by font_size for space between the text, defaults to 1.25
+    :param space: Multiplied by font_size for space under the text, defaults to 1.25
     :type space: float, optional
+    :param justified: How to align the image ("l":left, "c":center, "r":right), defaults to "c"
+    :type justified: str, optional
     :return: Image with the given text in multiple lines
     :rtype: PIL.Image
     """
+    # Get the basic image height
+    ref_image = get_text_line_image("abc", font=font, font_size=font_size,
+            image_width=100, space=space)
+    image_height = ref_image.size[1]
     # Create the base for the image
-    image_height = math.floor(font_size*space)
     background = Image.new("RGBA", size=(image_width, image_height*len(lines)), color=background)
     # Get a line image for each line of text
     for i in range(0, len(lines)):
@@ -242,4 +257,163 @@ def get_text_multiline_image(lines:List[str], font:ImageFont, font_size:int,
         background.alpha_composite(image, (0,i*image_height))
     return background
 
-def get_word_wrap(text:str, pixel_width:int, space:float=1.1):
+def get_word_wrap(text:str, font:ImageFont, image_width:int, minimum_characters:int) -> (List[str], int):
+    """
+    Takes a text string from the user and splits it into lines based on the minimum number of characters per line.
+    A value for the text size based on what can fit into a given width is also given.
+    The text size will be the largest size that the given font can be to fit all the lines in the given image width.
+    
+    :param text: Text string to split into individual lines
+    :type text: str, required
+    :param font: Font intended to be used when drawing text
+    :type font: ImageFont, required
+    :param image_width: Width of the image that is ment to have text applied
+    :type image_width: int, required
+    :param minimum_characters: The minimum number of characters that can be allowed on a single line
+    :type minimum_characters: int
+    :return: String split into lines, size of the font to fit the width
+    :rtype: (List[str], int)
+    """
+    # Get the longest character string in the given text
+    words = re.sub(r"\s+", " ", text).strip().split(" ")
+    words = sorted(words, key=len, reverse=True)
+    characters = len(words[0])
+    # Make sure the character length doesn't exceed the given minumum and maximum
+    if characters < minimum_characters:
+        characters = minimum_characters
+    # Create the wrapped text
+    lines = textwrap.wrap(text.strip(), width=characters, break_long_words=False)
+    # Set the conservative font size
+    font_size = math.floor(image_width/(characters*2))
+    # Get the line with the longest width
+    line_width = 0
+    largest_line = ""
+    for line in lines:
+        altered_font = font.font_variant(size=font_size)
+        ref_left, a, ref_right, b = altered_font.getbbox(line)
+        if ((ref_right+1) - ref_left) > line_width:
+            line_width = (ref_right+1) - ref_left
+            largest_line = line
+    # Get the maximum font size
+    while line_width < image_width:
+        font_size += 1
+        altered_font = font.font_variant(size=font_size)
+        ref_left, a, ref_right, b = altered_font.getbbox(largest_line)
+        line_width = (ref_right+1) - ref_left
+    font_size -= 1
+    # Return the lines and the font size
+    return (lines, font_size)
+
+def text_image_fit_width(text:str, font:ImageFont, image_width:int,
+            foreground:str="#000000ff", background:str="#ffffff00",
+            space:float=1.2, justified:str="c",
+            minimum_characters:int=1) -> Image:
+    """
+    Returns an image containing the given text in an image with a given width in pixels.
+    Text will be word-wrapped based on the minimum characters per line.
+    Text will fill as much horizontal space as possible, and vertical resolution is variable.
+    
+    :param lines: Text to write to the image
+    :type lines: Line[str], required
+    :param font: ImageFont to use for the text
+    :type font: PIL.ImageFont, required
+    :param image_width: Width of the generated image in pixels
+    :type image_width: int, required
+    :param foreground: Hex color value of the text in the generated image, defaults to "#000000ff"
+    :type foreground: str, optional
+    :param background: Hex color value of the generated image backround, defaults to "#ffffff00"
+    :type background: str, optional
+    :param space: Multiplied by font_size for space under the text, defaults to 1.25
+    :type space: float, optional
+    :param justified: How to align the image ("l":left, "c":center, "r":right), defaults to "c"
+    :type justified: str, optional
+    :param minimum_characters: The minimum number of characters allowed on a single line of text, defaults to 1
+    :type minimum_characters: int, optional
+    :return: Image with text, wrapped to fit the given image width
+    :rtype: PIL.Image
+    """
+    # Get the word wrap lines
+    lines, font_size = get_word_wrap(text, font, image_width, minimum_characters)
+    # Create the image
+    image = get_text_multiline_image(lines, font, font_size, image_width=image_width,
+            foreground=foreground, background="#00000000", space=space, justified=justified)
+    # Crop the image
+    left, top, right, bottom = get_bounds(image, "#00000000")
+    right = image.size[0]
+    cropped = image.crop((0, top, right, bottom))
+    # Add the background
+    background = Image.new("RGBA", size=(cropped.size), color=background)
+    background.alpha_composite(cropped, (0,0))
+    # Return the image
+    return background
+
+def text_image_fit_box(text:str, font:ImageFont, image_width:int, image_height:int,
+            foreground:str="#000000ff", background:str="#ffffff00",
+            space:float=1.2, justified:str="c", vertical:str="c",
+            minimum_characters:int=1) -> Image:
+    """
+    Returns an image containing the given text in within a given width and height.
+    Text will be word-wrapped based on the minimum characters per line.
+    Text will fill as much of the dimensions as possible.
+    
+    :param lines: Text to write to the image
+    :type lines: Line[str], required
+    :param font: ImageFont to use for the text
+    :type font: PIL.ImageFont, required
+    :param image_width: Width of the generated image in pixels
+    :type image_width: int, required
+    :param image_height: Height of the generated image in pixels
+    :type image_height: int, required
+    :param foreground: Hex color value of the text in the generated image, defaults to "#000000ff"
+    :type foreground: str, optional
+    :param background: Hex color value of the generated image backround, defaults to "#ffffff00"
+    :type background: str, optional
+    :param space: Multiplied by font_size for space under the text, defaults to 1.25
+    :type space: float, optional
+    :param justified: How to align the image ("l":left, "c":center, "r":right), defaults to "c"
+    :type justified: str, optional
+    :param vertical: How to align the image vertically ("t":top, "c":center, "b":bottom), defaults to "c"
+    :type vertical: str, optional
+    :param minimum_characters: The minimum number of characters allowed on a single line of text, defaults to 1
+    :type minimum_characters: int, optional
+    :return: Image with text, wrapped to fit the given image width and height
+    :rtype: PIL.Image
+    """
+    characters = minimum_characters - 1
+    calc_height = image_height + 100
+    while calc_height > image_height:
+        # Set the calculated_height and text wrap character values
+        characters += 1
+        calc_height = image_height + 100
+        # Get the word wrap lines
+        lines, font_size = get_word_wrap(text, font, image_width, characters)
+        low_font_size = math.floor(font_size * 0.9) - 1
+        font_size += 1
+        # Attempt to lower the font size until text fits in the box
+        while calc_height > image_height and (font_size > low_font_size or len(lines) == 1):
+            # Calculate the height of a line of text
+            font_size -= 1
+            altered_font = font.font_variant(size=font_size)
+            rl, ref_top, rr, ref_bottom = altered_font.getbbox(TEXT_REF)
+            line_height = (ref_bottom + 1) - ref_top
+            # Calculate the height of the image with this font size
+            calc_height = line_height + ((len(lines) - 1) * (line_height * space))
+    # Create the image
+    image = get_text_multiline_image(lines, font, font_size, image_width=image_width,
+            foreground=foreground, background="#00000000", space=space, justified=justified)
+    # Crop the image
+    left, top, right, bottom = get_bounds(image, "#00000000")
+    right = image.size[0]
+    cropped = image.crop((0, top, right, bottom))
+    # Add the background
+    background = Image.new("RGBA", size=(image_width, image_height), color=background)
+    cropped_height = cropped.size[1]
+    if vertical == "t":
+        y_position = 0
+    elif vertical == "b":
+        y_position = image_height - cropped_height
+    else:
+        y_position = math.floor((image_height - cropped_height) / 2)
+    background.alpha_composite(cropped, (0,y_position))
+    # Return the image
+    return background
